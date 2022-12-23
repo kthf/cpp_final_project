011d529ada40e136e6bf559eb1329348340484bb
kthf <55880280+kthf@users.noreply.github.com>
2022-12-23 23:49:49 +0500

    Update AVLTree.h

--- AVLTree.h
+++ AVLTree.h
@@ -8,7 +8,11 @@ template <typename T>
 class AVLTree : public AVLTreeBase<T> {
 protected:
     void balance(std::vector<AVLTreeNode<T>**> path) override;
+public:
+    std::vector<T> get_nodes_num();
 };
 
 #endif
+
+
 
