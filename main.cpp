d8d5cb6f4916fcfc57766f05b51db0bf5c291f2f
kthf <55880280+kthf@users.noreply.github.com>
2022-12-23 23:51:29 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ int main() {
         int x; std::cin >> x;
         root.insert(x);
     }
+    // root.display();
     // Получение элементов дерево, после вставки которых балансировка производиться не будет
-    auto v = root.inorder();
+    auto v = root.get_nodes_num();
+    std::vector<int> res;
     AVLTree<int> root2;
     std::cout << "Набор чисел, входящих в заданную последовательность на основе которой можно сформировать сбалансированное дерево без дополнительной балансировки:\n";
     for(auto &u : v) {
+        root2.insert(u);
         std::cout << u << " ";
     }
     std::cout << std::endl;
